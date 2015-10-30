43b713059da715be5d6d6e54b6d2c4d59a9bfc63
Stephan <stephan@freifunk-dresden.de>
2015-10-30 08:43:05 +0100

    install: add missing ubuntu public keys for some vserver enviroments

--- vserver-install.sh
+++ vserver-install.sh
@@ -32,6 +32,10 @@ chmod 755 $PREINSTALL
 > $POSTINSTALL
 chmod 755 $POSTINSTALL
 
+#add missing ubuntu public keys for some vserver
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 40976EAF437D05B5
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 3B4FE6ACC0B21F32
+
 apt-get update
 apt-get dist-upgrade
 
