56b7ea27da5f3cde91fc630c2863e48c30badc59
Stephan <freifunk@ddmesh.de>
2015-12-26 14:10:39 +0100

    conntrack: print values to stdout

--- files/usr/bin/conntrack.sh
+++ files/usr/bin/conntrack.sh
@@ -7,5 +7,6 @@ n1="$(/sbin/sysctl -a|grep -i 'net.netfilter.nf_conntrack_max')"
 n2="$(/sbin/sysctl -a|grep -i 'net.nf_conntrack_max')"
 c="$(/sbin/sysctl net.netfilter.nf_conntrack_count)"
 
-echo "conntrack: $d: $n1, $n2, $c " >> $f
+echo "conntrack: $d: $n1, $n2, $c" >> $f
+echo "conntrack: $d: $n1, $n2, $c"
 
